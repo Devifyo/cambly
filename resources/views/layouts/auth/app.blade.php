3530a32383aca5c6dadde0e4922f87c99890f6a7
SunilThagyal <sunilthagyal60@gmail.com>
2025-10-27 16:51:22 -0400

    fixes

---
+++ resources/views/layouts/auth/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Doccure Authentication Pages">
+    <meta name="keywords" content="login, register, doccure, doctor booking, authentication">
+    <meta name="author" content="Doccure Template - Authentication Layout">
+
+    <title>@yield('title', 'Doccure | Authentication')</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
+
+    <!-- Apple Touch Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/apple-touch-icon.png') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
+
+    <!-- Iconsax CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/iconsax.css') }}">
+
+    <!-- Feathericon CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/feather.css') }}">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+
+    @stack('styles')
+</head>
+
+<body class="@yield('body-class', 'login-body')">
+    <!-- Main Wrapper -->
+    <div class="main-wrapper">
+        @yield('content')
+    </div>
+    <!-- /Main Wrapper -->
+
+    <!-- Cursor -->
+    <div class="mouse-cursor cursor-outer"></div>
+    <div class="mouse-cursor cursor-inner"></div>
+    <!-- /Cursor -->
+
+    <!-- JS Files -->
+    <script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+
+    @stack('scripts')
+</body>
+</html>
